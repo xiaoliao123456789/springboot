3543b8eb8d349b3ef24497f9cabb96a24a02ae86
廖凌云 <lyliao0069@fiberhome.com>
2024-04-10 17:03:22 +0800

    123

---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,41 @@
+package com.example.demo.entity;
+
+public class User {
+    private String name;
+
+    private Integer id;
+    private String address;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
